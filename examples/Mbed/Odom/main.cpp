c18329b84374d7b5a26d52e671624e2e07dd478d
Yoshikawa Teru <teruyamato0731@icloud.com>
2022-11-24 00:19:08 +0900

    doc

--- examples/Mbed/Odom/main.cpp
+++ examples/Mbed/Odom/main.cpp
@@ -13,16 +13,21 @@ Odom<3> odom{};
 int main() {
   timer.start();
   while(1) {
-    // 時間を取得
-    auto now = timer.elapsed_time();
-    static auto pre = now;
-    auto delta = pre - now;
-
-    // エンコーダの値を引数に渡す
+    // エンコーダ角変位の変化量を引数に渡す
     odom.integrate({0, 0, 0});
-    auto pos = odom.get();
-    auto vel = pos / delta;
 
+    // 現在座標の取得
+    Coodinate pos = odom.get();
+
+    // 前回からの経過時間を取得
+    auto now = timer.elapsed_time();
+    static decltype(now) pre = {};
+    auto delta = pre - now;
     pre = now;
+
+    // 速度の取得
+    static Coodinate pre_pos = {};
+    Velocity vel = (pos - pre_pos) / delta;
+    pre_pos = pos;
   }
 }
