c66e278fe9506fddf598e8f58c940bae0e65800d
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-01-12 15:24:48 +0900

    ci: gcovの実行を省略

--- .github/scripts/ctest.bash
+++ .github/scripts/ctest.bash
@@ -3,4 +3,3 @@ mkdir -p build && cd "$_"
 cmake ..
 cmake --build . --target all --clean-first -j4
 ctest --rerun-failed --output-on-failure -j4 --output-junit report.xml
-gcovr --xml -o coverage.xml -e ".*test.*" -r ../
