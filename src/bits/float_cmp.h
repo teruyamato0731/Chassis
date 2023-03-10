e5273fa13e84d53d3f2aff1150b9eda5ff227284
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-03-10 15:27:48 +0900

    fix: float_cmpをimpl名前空間に秘匿

--- src/bits/float_cmp.h
+++ src/bits/float_cmp.h
@@ -5,6 +5,10 @@
 #include <cfloat>
 #include <cmath>
 
+namespace rct {
+
+namespace impl {
+
 inline int float_cmp(const float a, const float b) {
   const auto eps = FLT_EPSILON * std::max({1.0f, std::abs(a), std::abs(b)});
   if(a > b + eps) {
@@ -15,5 +19,10 @@ inline int float_cmp(const float a, const float b) {
     return 0;
   }
 }
+
+}  // namespace impl
+
+}  // namespace rct
+
 
 #endif  // RCT_FLOAT_CMP
