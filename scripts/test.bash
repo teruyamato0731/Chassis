30eed4e162c1622758de1017c360394a2bde089f
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-03-20 05:58:46 +0000

    ci: cmake のキャッシュを削除

--- scripts/test.bash
+++ scripts/test.bash
@@ -2,6 +2,7 @@
 set -euxo pipefail
 cd ${BASH_SOURCE[0]%/*}/..
 mkdir -p build && cd "$_"
+rm CMakeCache.txt || true
 cmake ..
 cmake --build . --target all --clean-first -j4
 ctest --rerun-failed --output-on-failure -j4 --output-junit report.xml
