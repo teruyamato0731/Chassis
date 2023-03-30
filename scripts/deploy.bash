6e9ae978431f3fd26bb938e83bc5ffb4cafc6bdc
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-03-30 03:31:56 +0000

    ci: submodule の checkout を script 内で実行

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 cd ${BASH_SOURCE[0]%/*}/..
-
+git submodule update --init docs/doxygen-custom/doxygen-awesome-css
 echo PROJECT_NUMBER set to ${PROJECT_VERSION:=$(git describe --tags --abbrev=0)}
 
 # doxygen docs/doxygen-custom/Doxyfile
--- scripts/test.bash
+++ scripts/test.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 cd ${BASH_SOURCE[0]%/*}/..
+git submodule update --init test/third_party/googletest
 mkdir -p build && cd "$_"
 rm CMakeCache.txt || true
 cmake ..
