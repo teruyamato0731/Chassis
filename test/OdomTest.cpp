f5a11a0a005867d7d0eb7197776642105b72ba4f
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-06-20 17:48:31 +0900

    feat!: Odomの初期位相を変更

--- test/OdomTest.cpp
+++ test/OdomTest.cpp
@@ -32,23 +32,42 @@ TEST(ODOM, SET) {
   EXPECT_EQ(p1, odom.get());
 }
 
-TEST(ODOM, INTEGRATE) {
-  {
-    Odom<3> odom{};
-    odom.integrate({0, 100, -100});
-    Coordinate expect{0, 173.205080757, 0};
-    auto actual = odom.get();
-    EXPECT_NEAR(expect.x_milli, actual.x_milli, 5e-5);
-    EXPECT_NEAR(expect.y_milli, actual.y_milli, 5e-5);
-    EXPECT_NEAR(expect.ang_rad, actual.ang_rad, 5e-5);
-  }
-  {
-    Odom<4> odom{};
-    odom.integrate({100, 0, -100, 0});
-    Coordinate expect{200, 0, 0};
-    auto actual = odom.get();
-    EXPECT_NEAR(expect.x_milli, actual.x_milli, 5e-5);
-    EXPECT_NEAR(expect.y_milli, actual.y_milli, 5e-5);
-    EXPECT_NEAR(expect.ang_rad, actual.ang_rad, 5e-5);
-  }
+TEST(ODOM, INTEGRATE_3_AHEAD) {
+  Odom<3> odom{};
+  odom.integrate({100, 0, -100});
+  Coordinate expect{0, 173.205080757, 0};
+  auto actual = odom.get();
+  EXPECT_NEAR(expect.x_milli, actual.x_milli, 5e-5);
+  EXPECT_NEAR(expect.y_milli, actual.y_milli, 5e-5);
+  EXPECT_NEAR(expect.ang_rad, actual.ang_rad, 5e-5);
+}
+
+TEST(ODOM, INTEGRATE_3_ROTATE) {
+  Odom<3> odom{};
+  odom.integrate({100, 100, 100});
+  Coordinate expect{0, 0, 300};
+  auto actual = odom.get();
+  EXPECT_NEAR(expect.x_milli, actual.x_milli, 5e-5);
+  EXPECT_NEAR(expect.y_milli, actual.y_milli, 5e-5);
+  EXPECT_NEAR(expect.ang_rad, actual.ang_rad, 5e-5);
+}
+
+TEST(ODOM, INTEGRATE_4_AHEAD) {
+  Odom<4> odom{};
+  odom.integrate({100, 100, -100, -100});
+  Coordinate expect{0, 282.842712475, 0};
+  auto actual = odom.get();
+  EXPECT_NEAR(expect.x_milli, actual.x_milli, 5e-5);
+  EXPECT_NEAR(expect.y_milli, actual.y_milli, 5e-5);
+  EXPECT_NEAR(expect.ang_rad, actual.ang_rad, 5e-5);
+}
+
+TEST(ODOM, INTEGRATE_4_ROTATE) {
+  Odom<4> odom{};
+  odom.integrate({100, 100, 100, 100});
+  Coordinate expect{0, 0, 400};
+  auto actual = odom.get();
+  EXPECT_NEAR(expect.x_milli, actual.x_milli, 5e-5);
+  EXPECT_NEAR(expect.y_milli, actual.y_milli, 5e-5);
+  EXPECT_NEAR(expect.ang_rad, actual.ang_rad, 5e-5);
 }
