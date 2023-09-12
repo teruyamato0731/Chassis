5562799ccaec57ae1fbaa313e71a74a41c6d1711
Yoshikawa Teru <teruyamato0731@gmail.com>
2023-09-12 11:50:25 +0900

    ci: doxygenのcacheを削除

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -4,6 +4,7 @@ cd "${BASH_SOURCE[0]%/*}"/..
 git submodule update --init docs/doxygen-custom/doxygen-awesome-css
 echo PROJECT_NUMBER set to "${PROJECT_VERSION:=$(git describe --tags --abbrev=0)}"
 
+rm -rf docs/html
 # doxygen docs/doxygen-custom/Doxyfile
 doxygen <(echo "PROJECT_NUMBER = ${PROJECT_VERSION}" | cat docs/doxygen-custom/Doxyfile -)
 
