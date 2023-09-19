a6389a373a2bad56a405e2918337f4fd05192f21
teruyamato0731 <teruyamato0731@users.noreply.github.com>
2023-09-19 07:22:51 +0000

    deploy: 2afb07b36503efd771f07256845e04a8e034f67c

--- group__pid.js
+++ group__pid.js
@@ -11,8 +11,8 @@ var group__pid =
     [ "rct::Pid< T >", "structrct_1_1Pid.html", [
       [ "Pid", "structrct_1_1Pid.html#aa9067283ace5f3b02b44282d77af922c", null ],
       [ "calc", "structrct_1_1Pid.html#a9ed66bff08d476a3b55875d1591377d2", null ],
-      [ "refresh", "structrct_1_1Pid.html#a0176e458aa7d30d001d68830186c1635", null ],
-      [ "set_pid_gain", "structrct_1_1Pid.html#a059a7d9693c998e090a37925554ad892", null ]
+      [ "refresh", "structrct_1_1Pid.html#a579cf6e4e0462ca14dccfefe5e4f239d", null ],
+      [ "set_pid_gain", "structrct_1_1Pid.html#ade69ba6b94df40c87c92303609dc02a4", null ]
     ] ],
     [ "rct::is_pidable_v", "group__pid.html#gaf6b0805eb269ff102f5a787ab5d5dc8f", null ]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['rct_0',['rct',['../namespacerct.html',1,'']]],
-  ['refresh_1',['refresh',['../structrct_1_1Pid.html#a0176e458aa7d30d001d68830186c1635',1,'rct::Pid']]]
+  ['refresh_1',['refresh',['../structrct_1_1Pid.html#a579cf6e4e0462ca14dccfefe5e4f239d',1,'rct::Pid']]]
 ];
